4e226d738e2e937fc15d29707395eeebcafce839
Harim Choi <harim1206@gmail.com>
2018-02-09 18:05:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,8 @@ def reverse_each_word(string)
 #{}"Hello, there and how are you?"
 #{}"olleH ,ereht dna woh era ?uoy"
 
+  puts("Mark****************************************************")
+
   wordArr = string.split(" ")
   reverseWordArr = []
 
