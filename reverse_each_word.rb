80eb4d991dfcad4cf777a16427ec22bf3a22149b
Harim Choi <harim1206@gmail.com>
2018-02-09 18:14:04 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+def reverse_each_word(string)
+#{}"Hello, there and how are you?"
+#{}"olleH ,ereht dna woh era ?uoy"
+
+  puts("Mark****************************************************")
+
+  wordArr = string.split(" ")
+
+  wordArr = wordArr.collect do |word|
+     word.reverse
+  end
+
+  puts("wordArr: #{wordArr}")
+
+  newStr = wordArr.join(" ")
+
+
+
+
+
+
+end
