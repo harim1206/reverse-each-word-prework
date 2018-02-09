afee07ba34da0f6ac054cda789a3dd6e029e1f32
Harim Choi <harim1206@gmail.com>
2018-02-09 18:04:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,16 @@
 def reverse_each_word(string)
-"Hello, there and how are you?"
-"olleH ,ereht dna woh era ?uoy"
+#{}"Hello, there and how are you?"
+#{}"olleH ,ereht dna woh era ?uoy"
 
-wordArr = string.split(" ")
-reverseWordArr = []
+  wordArr = string.split(" ")
+  reverseWordArr = []
 
-wordArr.each do |word|
-  reverseWordArr << word.reverse
-end
+  wordArr.each do |word|
+    reverseWordArr << word.reverse
+  end
 
-puts ("reverseWordArr")
-reverseWordArr
+  puts ("reverseWordArr")
+  reverseWordArr
 
 
 
